6b0f0b239a14c93a257e34f979d40021b30eefcb
HAJINHEE <hajinheee@gmail.com>
2022-06-03 15:41:50 +0900

    15:41

---
+++ linear_algebra/raccoon.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot as plt
+from scipy import misc  # 패키지 임포트
+from matplotlib import rc, font_manager
+rc('font', family=font_manager.FontProperties(fname='C:/Windows/Fonts/malgunsl.ttf').get_name())
+
+img_rgb = misc.face()  # 컬러 이미지 로드
+img_rgb.shape  # 데이터의 모양
+
+
+class Raccoon:
+
+    @staticmethod
+    def solution() -> None:
+        plt.subplot(221)
+        plt.imshow(img_rgb, cmap=plt.cm.gray)  # 컬러 이미지 출력
+        plt.axis("off")
+        plt.title("RGB 컬러 이미지")
+
+        plt.subplot(222)
+        plt.imshow(img_rgb[:, :, 0], cmap=plt.cm.gray)  # red 채널 출력
+        plt.axis("off")
+        plt.title("Red 채널")
+
+        plt.subplot(223)
+        plt.imshow(img_rgb[:, :, 1], cmap=plt.cm.gray)  # green 채널 출력
+        plt.axis("off")
+        plt.title("Green 채널")
+
+        plt.subplot(224)
+        plt.imshow(img_rgb[:, :, 2], cmap=plt.cm.gray)  # blue 채널 출력
+        plt.axis("off")
+        plt.title("Blue 채널")
+
+        print(plt.show())
+
+
+if __name__ == '__main__':
+    Raccoon.solution()
