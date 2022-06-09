e0d5431fc70d60ebfc923a080e6e460e889178bf
HAJINHEE <hajinheee@gmail.com>
2022-06-09 18:12:21 +0900

    users 테이블 생성

--- users/models.py
+++ users/models.py
@@ -5,9 +5,9 @@ class User(models.Model):
     use_in_mygrations = True
     username = models.CharField(primary_key=True, max_length=10)
     password = models.CharField(max_length=10)
-    name = models.CharField
-    email = models.CharField
-    regDate = models.CharField
+    name = models.TextField()
+    email = models.TextField()
+    regDate = models.DateField()
 
     class Meta:
         db_table = "users"
