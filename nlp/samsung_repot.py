0b0ddfa46f83a97a5bf5a9996bb5cdfef57aa025
HAJINHEE <hajinheee@gmail.com>
2022-06-14 16:21:08 +0900

    samsung repot

--- nlp/samsung_repot.py
+++ nlp/samsung_repot.py
@@ -10,8 +10,23 @@ from icecream import ic
 
 from context.domains import File, Reader
 '''
-https://blog.diyaml.com/teampost/%EC%9E%90%EC%97%B0%EC%96%B4-%EC%B2%98%EB%A6%AC%EC%9D%98-4%EA%B0%80%EC%A7%80-%EB%8B%A8%EA%B3%84/
-1. Preprocessing
+문장 형태의 문자 데이터를 전처리할 때 많이 사용되는 방법이다. 
+말뭉치(코퍼스 corpus)를 어떤 토큰의 단위로 분할하냐에 따라 
+단어 집합의 크기, 단어 집합이 표현하는 토크의 형태가 다르게 나타나며 
+이는 모델의 성능을 좌지우지하기도 한다. 
+이때 텍스트를 토큰의 단위로 분할하는 작업을 토큰화라고 한다. 
+토큰의 단위는 보통 의미를 가지는 최소 의미 단위로 선정되며, 
+토큰의 단위를 단어로 잡으면 Word Tokenization이라고 하고, 
+문장으로 잡으면 Sentence Tokeniazation이라고 한다. 
+
+영어는 주로 띄어쓰기 기준으로 나누고, 
+한글은 단어 안의 형태소를 최소의미 단위(=atom)로 인식해 적용한다.
+
+형태소(形態素, 영어: morpheme)는 언어학에서 의미가 있는 가장 작은 말의 단위이다.
+
+코퍼스(영어: corpus) 말뭉치는 언어학에서 주로 구조를 이루고 있는 텍스트 집합이다.
+
+1. Preprocessing : kr-Report_2018.txt 를 읽는다.
 2. Tokenization
 3. Token Embedding
 4. Document Embedding
@@ -31,6 +46,9 @@ class Solution(Reader):
             print('3. Token Embedding')
             print('4. Document Embedding')
             print('5. 2018년 삼성사업계획서를 분석해서 워드클라우드를 작성하시오.')
+            print('6. Stopword')
+            print('7. Remove Stopword')
+            print('9. nltk 다운로드.')
             return input('메뉴 선택 \n')
 
         while 1:
@@ -38,7 +56,7 @@ class Solution(Reader):
             if menu == '0':
                 break
             if menu == '1':
-                self.read_file()
+                self.preprocessing()
             if menu == '2':
                 self.tokenization()
             if menu == '3':
@@ -47,21 +65,59 @@ class Solution(Reader):
                 self.document_embedding()
             if menu == '5':
                 pass
+            if menu == '6':
+                self.read_stopword()
+            if menu == '7':
+                self.remove_stopword()
+            if menu == '9':
+                Solution.download()
             elif menu == '0':
                 break
 
-    def read_file(self):
+    @staticmethod
+    def download():
+        nltk.download('punkt')
+        nltk.download('stopwords')
+
+    def preprocessing(self):
         self.okt.pos("삼성전자 글로벌센터 전자사업부", stem=True)
         file = self.file
         file.fname = 'kr-Report_2018.txt'
         path = self.new_file(file)
         with open(path, 'r', encoding='utf-8') as f:
             texts = f.read()
-        print(texts)
-        return texts
+        texts = texts.replace('\n', ' ')
+        tokenizer = re.compile(r'[^ㄱ-힣]+')
+        return tokenizer.sub(' ', texts)
 
     def tokenization(self):
-        pass
+        noun_tokens = []
+        tokens = word_tokenize(self.preprocessing())
+        # ic(tokens[:100])
+        for i in tokens:
+            pos = self.okt.pos(i)
+            _ = [j[0] for j in pos if j[1] == 'Noun']
+            if len(''.join(_)) > 1:
+                noun_tokens.append(' '.join(_))
+        texts = ' '.join(noun_tokens)
+        # ic(texts[:100])
+        return texts
+
+    def read_stopword(self):
+        self.okt.pos("삼성전자 글로벌센터 전자사업부", stem=True)
+        file = self.file
+        file.fname = 'stopwords.txt'
+        path = self.new_file(file)
+        with open(path, 'r', encoding='utf-8') as f:
+            texts = f.read()
+        # ic(texts)
+        return texts
+
+    def remove_stopword(self):
+        tokens = self.tokenization()
+        stopwords = self.read_stopword()
+        texts = [i for i in tokens.split() if i not in stopwords.split()]
+        ic(texts)
 
     def token_embedding(self):
         pass
