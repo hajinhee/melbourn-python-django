0cfe9045662c32481c42cf4dfc43000ed6d140f3
HAJINHEE <hajinheee@gmail.com>
2022-06-29 11:57:53 +0900

    11:57

--- tf2/hand_writing2.py
+++ tf2/hand_writing2.py
@@ -16,6 +16,7 @@ class Solution(tf.keras.Model):
         self.val_ds = None
         self.x_test = None
         self.y_test = None
+        self.hist = None
 
     def preprocessing(self):
         # MNIST 데이터셋 가져오기
@@ -43,7 +44,7 @@ class Solution(tf.keras.Model):
         # model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
         # 모델 학습
-        hist = model.fit(self.train_ds, validation_data=self.val_ds, epochs=10)
+        self.hist = model.fit(self.train_ds, validation_data=self.val_ds, epochs=10)
 
         # 모델 평가
         print('모델 평가')
@@ -51,10 +52,26 @@ class Solution(tf.keras.Model):
 
         # 모델 정보 출력
         model.summary()
+        model.save('./save/mnist_model.h5')
+
+    def draw(self):
+        fig, loss_ax = plt.subplots()
+        acc_ax = loss_ax.twinx()
+        hist = self.hist
+        loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+        loss_ax.plot(hist.history['val_loss'], 'r', label='val loss')
+        acc_ax.plot(hist.history['accuracy'], 'b', label='train acc')
+        acc_ax.plot(hist.history['val_accuracy'], 'g', label='val acc')
+        loss_ax.set_xlabel('epoch')
+        loss_ax.set_ylabel('loss')
+        acc_ax.set_ylabel('accuracy')
+        loss_ax.legend(loc='upper left')
+        acc_ax.legend(loc='lower left')
+        plt.show()
 
 
 if __name__ == '__main__':
     s = Solution()
-    s.preprocessing()
-    s.modeling()
+    # s.preprocessing()
+    # s.modeling()
 
